c59b985279d9af8df0d22f3180ff18ec3cb6a461
JADEWANG <jbwang@mtu.edu>
2021-09-22 11:21:42 -0400

    implementing recursive Fibonacci method

--- prog1.c
+++ prog1.c
@@ -5,3 +5,11 @@ int main(int argc, char* argv[]) {
     printf("Hello, World!\n");
     return 0;
 }
+
+long fibonacci(int n) {
+    if (n < 2) {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
