64c9d75959b08e2b85e33e4742e18571c8642023
octillionbees <jbwang@mtu.edu>
2021-09-26 17:21:02 -0400

    implemented ellipse method

--- prog1.c
+++ prog1.c
@@ -15,13 +15,13 @@ int main(int argc, char* argv[]) {
     int r = atoi(argv[2]);
     int a = atoi(argv[3]);
     int b = atoi(argv[4]);
-    long s = atoi(argv[5]);
+    int s = atoi(argv[5]);
     int x = atoi(argv[6]);
     int y = atoi(argv[7]);
 
     printf("Fibonacci Input            = %ld", n);
     printf("Buffon's Needle Iterations = %d", r);
-    printf("Total random Number Pairs  = %ld", s);
+    printf("Total random Number Pairs  = %d", s);
     printf("Semi-Major Axis Length     = %d", a);
     printf("Semi-Minor Axis Length     = %d", b);
     printf("Number of Bins             = %d", x);
@@ -57,4 +57,24 @@ float buffon(int r) {
     }
 
     return t/r;
+}
+
+float ellipse(int a, int b, int s) {
+    //s = iterations of problem
+    int t = 0;
+    float x;
+    float y;
+
+    for (int i = 0; i < s; i++) {
+        //generate a random point in the rectangle bounded by the x and y axes, and the lines x = a, y = b
+        x = ((float rand())/RAND_MAX) * a;
+        y = ((float rand())/RAND_MAX) * b;
+
+        float result = powf(x, 2)/pow(a, 2) + powf(y, 2)/pow(b, 2);
+        if (result <= 1) {
+            t++;
+        }
+    }
+
+    return ((t/s) * a * b) * 4;
 }
